d9c465304ae3e76b2c0bbf0262c471aaca139fc6
Calvin Shin <calvin.shin@gmail.com>
2019-07-20 22:18:20 -0700

    Added configs for deployment

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 // Mongo initialize
 var mongoose = require("mongoose");
 var db = require("../models");
-mongoose.connect('mongodb://localhost/scrapernotes', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/scrapernotes', {useNewUrlParser: true});
 
 module.exports = db;
