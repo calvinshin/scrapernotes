d7f7c35efcf6f859c860aa8a0b96b46501e94a30
Calvin Shin <calvin.shin@gmail.com>
2019-07-22 00:16:16 -0700

    Added

--- server/db.js
+++ server/db.js
@@ -1,7 +1,8 @@
 // Mongo initialize
 var mongoose = require("mongoose");
 var db = require("../models");
-MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/scrapernotes'
+MONGODB_URI = process.env.MONGODB_URI || "mongodb://<dbuser>:<dbpassword>@ds353457.mlab.com:53457/heroku_r0h0bj3v"
+// 'mongodb://localhost/scrapernotes' || 
 mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
 
 module.exports = db;
